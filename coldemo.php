b3c0cded4fc51cf817621b84007472a72c365c39
Dev Redoy <mdredoyislam84@gmail.com>
2024-08-01 21:43:33 +0600

    Final Plugins Update

--- coldemo.php
+++ coldemo.php
@@ -285,3 +285,31 @@ function coldemo_wc_filter_data($wpquery){
     }
 }
 add_action('pre_get_posts', 'coldemo_wc_filter_data');
+
+function wordcount_count_words($content){
+    $stripped_content = strip_tags($content);
+    $wordn = str_word_count($stripped_content);
+    $label = __('Total Number of Words', 'wordcount');
+    $label = apply_filters('wordcount_heading', $label);
+    $tag = apply_filters('wordcount_tag','h2');
+    $content .= sprintf('<%s>%s:%s</%s>', $tag,$label, $wordn,$tag);
+    return $content;
+}
+add_filter('the_content', 'wordcount_count_words');
+
+function wordcount_reading_time($content){
+    $stripped_content = strip_tags($content);
+    $wordn = str_word_count($stripped_content);
+    $reading_munites = floor($wordn / 200);
+    $reading_secounds = floor($wordn % 200 / (200/60));
+
+    $is_visible = apply_filters('wordcoun_display_readingtime', 1);
+    if($is_visible){
+        $label = __('Total Reading Time', 'wordcount');
+        $label = apply_filters('wordcount_readingtime_heading', $label);
+        $tag = apply_filters('wordcount_readingtime_tag','h4');
+        $content .= sprintf('<%s>%s:%s minutes %s secounds</%s>', $tag,$label, $reading_munites, $reading_secounds,$tag);
+    }
+    return $content;
+}
+add_filter('the_content', 'wordcount_reading_time');
